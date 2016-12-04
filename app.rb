39aca3322a417942464aa20c66be57a8d6b4de0f
karpovVS <kazankarpov@gmail.com>
2016-12-04 14:38:12 +0300

    Add files via upload

---
+++ app.rb
@@ -0,0 +1,37 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+    erb :about
+end
+
+get'/visit' do
+	erb :visit
+end
+
+post '/visit' do #обработка запроса
+
+@username = params[:username]
+@contact_phone = params[:contact_phone]
+@visit_time = params[:visit_time]
+@barber = params[:barber]
+@color = params[:color]
+
+
+erb "OK,username is #{@username},
+phone number #{@contact_phone}, 
+time #{@visit_time} Цурюльнег будет #{@barber} цвет #{@color}!"
+
+end
+
+
+
+#get '/contacts' do
+ # erb :contacts
+#end`
+
